b2a593fbfa4b67275b6a7ba348df32e11860a585
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-22 19:39:01 -0400

    initialize and self all pass

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Add your code here
+class Dog
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        #clear array @@all of all Dog instances
+    end
+end
