0b9bbf82f5dccab0ed20bd508c0b8abe05f674a6
flatironschool <flatironschool@flatironschools-MacBook-Air.local>
2019-09-24 21:31:17 -0400

    save not passing tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,14 +20,21 @@ class Dog
         #clear array @@all of all Dog instances
         @@all.clear
     end
-    #binding.pry
-
+    
     def self.print_all
         #print each name of dog to the terminal
         @@all.each do |each_dog|
             puts each_dog.name
         end
-
         #p name is "Dog"
+    end
+    
+    def save
+        @@all.find do |dog_name|
+            @@all.dog_name == dog_name
+            binding.pry
+            "save this dog"
+        end
+        #@@all << # dog instance to array
     end
 end
